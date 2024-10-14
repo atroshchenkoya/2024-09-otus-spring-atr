9ac43cba6e5f820a3853166fae2d24992cd0ee84
atroshchenkoya <dragonsflames@yandex.ru>
2024-10-14 23:00:39 +0300

    read from test res add + test rename

--- hw02-annotation-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw02-annotation-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -25,7 +25,7 @@ class TestServiceImplTest {
     StreamsIOService ioService;
 
     @Test
-    public void doTest() {
+    void countOfRightAnswersShouldBeCorrect() {
 
         TestServiceImpl testService = new TestServiceImpl(ioService, questionDao);
 
